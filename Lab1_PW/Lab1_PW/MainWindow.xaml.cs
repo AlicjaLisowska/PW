9f0f68ec248038e2e0076ee7eec8c1de5e37fa9a
AlicjaLisowska <61700083+AlicjaLisowska@users.noreply.github.com>
2021-10-11 16:47:46 +0200

    Add files via upload

---
+++ Lab1_PW/Lab1_PW/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Lab1_PW
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DispatcherTimer Timer = new DispatcherTimer();
+            Timer.Interval = TimeSpan.FromSeconds(1);
+            Timer.Tick += timer_Tick;
+            Timer.Start();
+
+        }
+        void timer_Tick(object sender, EventArgs e)
+        {
+            Timer.Content = DateTime.Now.ToLongTimeString();
+        }
+
+        private void Stoper_Click(object sender, RoutedEventArgs e)
+        {
+            var Stoper = new Window();
+            Stoper.Show();
+            this.Close();
+        }
+    }
+
+  
+}
