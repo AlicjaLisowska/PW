7ac1890dfa9d2d5783cb8bde1d72b7156f511e3c
AlicjaLisowska <61700083+AlicjaLisowska@users.noreply.github.com>
2021-10-25 16:40:48 +0200

    Add files via upload

---
+++ LAB3_PW/LAB03_WF/LAB03_WF/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace LAB03_WF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public int id = 0;
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+        }
+
+     
+
+        private void addNew_Click(object sender, RoutedEventArgs e)
+        {
+
+             Window1 newWindow = new Window1(this);
+            newWindow.Show();
+
+        }
+   
+    }
+    public class Row
+    {
+        public string Name { get; set; }
+
+        public int ID { get; set; }
+
+        public int Count { get; set; }
+    }
+}
---
+++ LAB3_PW/LAB03_WF/LAB03_WF/Window1.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LAB03_WF
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        MainWindow mainwindow;
+
+      
+        public Window1(MainWindow mainwindow)
+        {
+            
+            this.mainwindow = mainwindow;
+            InitializeComponent();
+      
+     
+            
+        }
+       
+         
+        private void add_Click(object sender, RoutedEventArgs e)
+        {
+         mainwindow.id += 1;
+            List<Row> items = new List<Row>();
+            items.Add(new Row() { Name = nameTextbox.Text, ID =mainwindow.id, Count = int.Parse(countTextbox.Text) });
+        
+            mainwindow.Items.ItemsSource = items;
+        
+            this.Close();
+        }
+      
+    }
+}
