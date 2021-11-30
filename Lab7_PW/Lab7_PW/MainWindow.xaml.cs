7781bf92f4c94e0b67552670888bb2905cd2bb13
AlicjaLisowska <61700083+AlicjaLisowska@users.noreply.github.com>
2021-11-30 14:04:28 +0100

    Completed project
    When  load/save file, first file presents data with readers and second file presents data with books

--- Lab7_PW/Lab7_PW/MainWindow.xaml.cs
+++ Lab7_PW/Lab7_PW/MainWindow.xaml.cs
@@ -17,7 +17,7 @@ using System.IO;
 using System.Windows;
 using Microsoft.Win32;
 using System.Xml.Serialization;
-using System.Xml;
+
 
 
 namespace Lab7_PW
@@ -27,12 +27,36 @@ namespace Lab7_PW
     /// </summary>
     public partial class MainWindow : Window
     {
+        
         public int id;
         List<Row> items = new List<Row>();
         List<Books> booksList = new List<Books>();
         public MainWindow()
         {
             InitializeComponent();
+            //load readers
+            OpenFileDialog openFile = new OpenFileDialog();
+            if (openFile.ShowDialog() == true)
+            {
+                items.Clear();
+           
+                XmlSerializer xs = new XmlSerializer(typeof(List<Row>));
+                Stream s = File.OpenRead(openFile.FileName);
+                items = (List<Row>)xs.Deserialize(s);
+                User.ItemsSource = items;
+
+            }
+
+            //Load books
+            OpenFileDialog openFile2 = new OpenFileDialog();
+            if (openFile2.ShowDialog() == true)
+            {
+                booksList.Clear();
+                XmlSerializer bs = new XmlSerializer(typeof(List<Books>));
+                Stream s2 = File.OpenRead(openFile2.FileName);
+                booksList = (List<Books>)bs.Deserialize(s2);
+                Book.ItemsSource = booksList;
+            }
         }
 
         private void aUser_Click(object sender, RoutedEventArgs e)
@@ -62,7 +86,6 @@ namespace Lab7_PW
             items.Add(new Row() { Name = n, Surname = s,ID = id });
             User.ItemsSource = items;
             User.Items.Refresh();
-
         }
 
         private void aBook_Click(object sender, RoutedEventArgs e)
@@ -98,6 +121,35 @@ namespace Lab7_PW
             Book.Items.Refresh();
             Book.UnselectAll();
             User.UnselectAll();
+        }
+        private void Dialog_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Do you want save changes?", "Warning", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (result == MessageBoxResult.No)
+            {
+                e.Cancel = false;
+            }
+            else if (result == MessageBoxResult.Yes)
+            {
+               //save readers
+                XmlSerializer xs = new XmlSerializer(typeof(List<Row>));
+                SaveFileDialog sFile = new SaveFileDialog();
+                if (sFile.ShowDialog() == true)
+                {
+                    TextWriter txtWriter = new StreamWriter(sFile.FileName + ".xml");
+                    xs.Serialize(txtWriter, items);
+                    txtWriter.Close();
+                }
+                //save books
+                XmlSerializer bs = new XmlSerializer(typeof(List<Books>));
+                SaveFileDialog sFile2 = new SaveFileDialog();
+                if (sFile2.ShowDialog() == true)
+                {
+                    TextWriter txtWriter2 = new StreamWriter(sFile2.FileName + ".xml");
+                    bs.Serialize(txtWriter2, booksList);
+                    txtWriter2.Close();
+                }
+            }
         }
     }
 }
